0bac94437c925baa5086455fec982a0edc71c09a
Rohan T Kini <138154364+RohanKini18@users.noreply.github.com>
2024-07-08 23:05:05 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  
+
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
