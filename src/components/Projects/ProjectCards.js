b596058542de17a3e9224e5089dc75b5e11c909d
Rohan T Kini <138154364+RohanKini18@users.noreply.github.com>
2025-06-06 16:45:31 +0530

    Update v3.4

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -10,7 +10,7 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title style={{  fontSize: 22 }}>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
